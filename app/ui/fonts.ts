4b13a2d31319c6ff9d3d0224fe2ac8d370074fb9
TaiwoEnoch <taiwoenoch0@gmail.com>
2024-10-07 18:25:49 +0100

    next JS

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+// Specify weights for each font
+export const inter = Inter({ 
+  subsets: ['latin'], 
+  weight: ['400', '700'] // Add the weight property
+});
+
+export const lusitana = Lusitana({ 
+  subsets: ['latin'], 
+  weight: ['400', '700'] // Add the weight property
+});
